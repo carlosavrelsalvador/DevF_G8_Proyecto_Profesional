92b87e781a822988c0754e44017f71e82a3c8213
carlos vasquez <carlosavrelsalvador@gmail.com>
2021-10-06 01:34:03 -0600

    ficha de usuario actual

--- src/components/core/Profile.jsx
+++ src/components/core/Profile.jsx
@@ -1,8 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Typography } from "@mui/material";
 import getInfoUser from "../../services/User.services";
+// import @mui
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import { red } from '@mui/material/colors';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+// fin import @mui
+
+
 
 const Profile = () => {
+  const [profileData, setProfileData] = useState(false);
+
   useEffect(() => {
     // Recuperar informacion de persona loggeada desde endpoint
     getInfoProfile();
@@ -10,11 +23,13 @@ const Profile = () => {
 
   const getInfoProfile = async () => {
     const hardCodedJWT =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYmM1NDVmOWQ1OTE4NGUwMDA3MjEwMSIsInJvbGUiOiJDVVNUT01FUiIsImV4cCI6MTYzMzQ3ODc1OSwiaWF0IjoxNjMzMzkyMzU5fQ.pKfiEAkWKRbWiR8x3HA7xVTLWPjt_N6z9m9Ss5ILL84";
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNTc0ODhjMTdjNmVkMDAxNzE2YTM1MCIsInJvbGUiOiJBRE1JTiIsImV4cCI6MTYzMzU4NjI4OSwiaWF0IjoxNjMzNDk5ODg5fQ.DVfh4rGgJ6e_2CUI0na9kVU6QTjYfMgPylsIBWaySZU";
 
     try {
       const result = await getInfoUser(hardCodedJWT);
-      console.log("result", result);
+      console.log("getInfoUser result", result);
+      setProfileData({ data: result })
+      console.log(profileData.data.role)
     } catch (error) {
       console.log("error al recuperar info", error);
     }
@@ -23,7 +38,39 @@ const Profile = () => {
   return (
     <div>
       <Typography variant="h6" style={{ textAlign: "center", color: "orange" }}>
-        Mi Perfil
+        <div style={{ display: "inline-block" }}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  U
+                </Avatar>
+              }
+              action={
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title="Ficha de Usuario"
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                USUARIO {profileData.data?.user?.last_name} {profileData.data?.user?.first_name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                ROL DE USUARIO {profileData.data?.user?.role}
+              </Typography>
+
+              <Typography variant="body2" color="text.secondary">
+                EMAIL {profileData.data?.user?.email}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                GENERO {profileData.data?.user?.gender}
+              </Typography>
+            </CardContent>
+
+          </Card>
+        </div>
       </Typography>
     </div>
   );
